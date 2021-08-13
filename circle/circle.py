445ef4aaefac224b86dd18bd789f96cfcb659515
Al Rempel <alex@rmpl.of.by>
2021-08-13 14:22:16 +0300

    complete circle and pizza_delivery exercises :tada:

--- circle/circle.py
+++ circle/circle.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
-
-# Бабушка Зина любит печь блины своему любимому внуку Васе.
-# А внук Вася любит математику и знает, что периметр и площадь блина можно найти по диаметру сковородки.
-# Напишите программу, которая поможет Васе проверить его вычисления.
-
+"""
+ Бабушка Зина любит печь блины своему любимому внуку Васе. А внук Вася любит
+ математику и знает, что периметр и площадь блина можно найти по диаметру
+ сковородки. Напишите программу, которая поможет Васе проверить его
+ вычисления.
+"""
+import sys
 from math import pi
 
 
-def perimeter(d):
-    return pi * d
+def check_diameter_value(diameter):
+    if diameter < 0:
+        raise ValueError(f"Negative diameter ({diameter}) is invalid")
 
 
-def square(d):
-    r = d / 2
-    return pi * r ** 2
+def perimeter(diameter):
+    check_diameter_value(diameter)
+    return pi * diameter
+
+
+def aria(diameter):
+    check_diameter_value(diameter)
+    raduis = diameter / 2
+    return pi * pow(raduis, 2)
 
 
 if __name__ == "__main__":
-    d = int(input("Диаметр сковородки = "))
+    d = int(input("Диаметр сковороды = "))
 
-    print(perimeter(d))
-    print(square(d))
+    try:
+        pan_perimeter = perimeter(d)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
+    print(f"Периметр сковороды:\t{pan_perimeter:>10.2f} ед")
+
+    pan_aria = aria(d)
+    print(f"Площадь сковороды: \t{pan_aria:>10.2f} кв. ед")
--- pizza_delivery/pizza.py
+++ pizza_delivery/pizza.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python
 
-# В. Пупкин развозит пиццу. Ему сообщают адрес доствки в виде
-# <улица> <номер дома>-<номер квартиры>
-#
-# Приезжая по указанному адресу, Вася видит f-этажное здание.
-# Для простоты будем считать, что на каждом этаже в подъезде находятся 4 квартиры.
-#
-# Помогите Васе посчитать, в каком подъезде и на каком этаже находится нужная квартира n.
-#
-# Для решения понадобится использовать деление по модулю %
-# или целочисленное деление //.
+"""
+В. Пупкин развозит пиццу. Ему сообщают адрес доствки в виде <улица> <номер
+дома>-<номер квартиры>
+
+Приезжая по указанному адресу, Вася видит f-этажное здание. Для простоты будем
+считать, что на каждом этаже в подъезде находятся 4 квартиры.
+
+Помогите Васе посчитать, в каком подъезде и на каком этаже находится нужная
+квартира n.
+
+Для решения понадобится использовать деление по модулю % или целочисленное
+деление //.
+"""
+from math import ceil
+
+FLATS_PER_FLOOR = 4
 
 
-def find_entrance(f, n):
+def flats_per_entrance(building_height):
+    return FLATS_PER_FLOOR * building_height
+
+
+def find_entrance(building_height, apart_num):
     """
-    f - число этажей в доме
-    n - номер квартиры
+    building_height - число этажей в доме
+    apart_num       - номер квартиры
     """
+    return ceil(apart_num / flats_per_entrance(building_height))
 
-    return 0
 
-
-def find_floor(f, n):
+def find_floor(building_height, apart_num):
     """
-    f - число этажей в доме
-    n - номер квартиры
+    building_height - число этажей в доме
+    apart_num       - номер квартиры
     """
-
-    return 0
+    apart_num_in_ent = apart_num % flats_per_entrance(building_height)
+    if apart_num_in_ent == 0:
+        return building_height
+    return ceil(apart_num_in_ent / FLATS_PER_FLOOR)
 
 
 if __name__ == "__main__":
@@ -35,5 +46,5 @@ if __name__ == "__main__":
     flat_num = int(input("Номер квартиры: "))
 
     entrance = find_entrance(floors, flat_num)
-    floor = find_floor(floors, flat_num)
-    print(entrance, floor)
+    ent_floor = find_floor(floors, flat_num)
+    print(entrance, ent_floor)
