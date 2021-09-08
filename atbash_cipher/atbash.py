391963841360b74c0c12e01a1b70131ac0ec2dc4
Al Rempel <alex@rmpl.of.by>
2021-09-08 18:54:53 +0300

    complete `atbash` :tada:

--- atbash_cipher/atbash.py
+++ atbash_cipher/atbash.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 """
-Напишите программу, которая кодирует и декодирует текст шифром Атбаш.
-В этом шифре каждая i-ая буква алфавита заменяется i-ой буквой с его конца, например, для латинского алфавита: a - z, b - y и т.д.
+Напишите программу, которая кодирует и декодирует текст шифром Атбаш. В этом
+шифре каждая i-ая буква алфавита заменяется i-ой буквой с его конца,
+например, для латинского алфавита: a - z, b - y и т.д.
 
 - заглавные буквы переводятся в строчные
 - пробельные символы и знаки препинания опускаются
@@ -12,13 +13,39 @@
 
 `Bambarbia, Kirgudu` -> `yznyz iyrzp ritfw f`
 """
+from string import ascii_letters
+from typing import Generator
 
 BLOCK_SIZE = 5
 
+TRANS = str.maketrans(
+    ascii_letters[:26],  # noqa: WPS432
+    str.join('', reversed(ascii_letters[:26])),  # noqa: WPS432
+    )
+
 
 def atbash_encode(text: str) -> str:
-    pass
+    encode_gen = atbash_encode_gen(text)
+    return str.join(' ', encode_gen)
+
+
+def atbash_encode_gen(text: str) -> Generator:
+    text_iter = filter(filter_predicate, map(str.lower, text))
+    chunk_iter = (iter(text_iter),) * BLOCK_SIZE
+    while True:
+        chunk = str.join('', map(next, chunk_iter))
+        if not chunk:
+            break
+        yield translator(chunk)
 
 
 def atbash_decode(cipher: str) -> str:
-    pass
+    return str.translate(str.replace(cipher, ' ', ''), TRANS)
+
+
+def translator(char: str) -> str:
+    return str.translate(char, TRANS)
+
+
+def filter_predicate(char):
+    return str.isalpha(char) or str.isnumeric(char)
