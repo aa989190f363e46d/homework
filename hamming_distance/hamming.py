30e8385c40d252c0638b6ce16c86c099e010f63a
Al Rempel <alex@rmpl.of.by>
2021-09-10 23:09:58 +0300

    :heavy_check_mark: `hamming`

--- hamming_distance/hamming.py
+++ hamming_distance/hamming.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 """
-Напишите программу, которая рассчитывает расстояние Хэмминга (Hamming distance) двух цепочек ДНК.
+Напишите программу, которая рассчитывает расстояние Хэмминга(Hamming distance)
+двух цепочек ДНК.
 
 ДНК задаётся 4 нуклеотидами:
 
-1. аденин - A
-2. цитозин - C
-3. гуанин - G
-4. тимин - T
+1. аденин - A 2. цитозин - C 3. гуанин - G 4. тимин - T
 
-Расстояние Хэмминга оперделяет число отличающихся нуклеотидов, находящихся в одинаковой позиции:
+Расстояние Хэмминга оперделяет число отличающихся нуклеотидов, находящихся в
+одинаковой позиции:
 
 strand_a = GAGCCTACTAACGGGAT
 strand_b = CATCGTAATGACGGCCT
@@ -22,6 +21,5 @@ strand_b = CATCGTAATGACGGCCT
 
 def hamming_distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
-        raise ValueError("Цепочки ДНК должны быть одинаковой длины")
-
-    pass
+        raise ValueError('Цепочки ДНК должны быть одинаковой длины')
+    return sum((1 for lt_a, lt_b in zip(strand_a, strand_b) if lt_a != lt_b))
