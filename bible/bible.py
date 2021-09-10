787536c8fbee56d9b9136029a48f4198cb478955
Al Rempel <alex@rmpl.of.by>
2021-09-10 23:05:27 +0300

    :heavy_check_mark: `bible`

--- bible/bible.py
+++ bible/bible.py
@@ -4,18 +4,20 @@
 Напишите функцию, которая находит n наиболее употребляемых слов из Библии.
 Слова в возвращаемом списке идут по убыванию их частоты использования.
 """
-
-
+from collections import Counter
+from itertools import islice
 from pathlib import Path
 
+kjb_file = Path(__file__).parent.absolute() / 'king_james_bible.txt'
+BIBLE = kjb_file.read_text()
 
-f = Path(__file__).parent.absolute() / "king_james_bible.txt"
-BIBLE = f.read_text()
+TOP_WORDS_COINT = 10
 
 
 def most_freq_bible_words(n: int) -> list:
-    pass
+    kjb_word_couner = Counter(BIBLE.lower().split())
+    return [word for word, _ in kjb_word_couner.most_common(n)]
 
 
 if __name__ == "__main__":
-    print(*most_freq_bible_words(10), sep="\n")
+    print(*most_freq_bible_words(TOP_WORDS_COINT), sep='\n')
