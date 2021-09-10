8f9fc9a0cef1483076dc3fd1515fbb365f356e5d
Al Rempel <alex@rmpl.of.by>
2021-09-10 22:50:23 +0300

    :heavy_check_mark: `fibonacci`

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -5,11 +5,32 @@
 Разрешается использовать временные переменные, циклы и условные операторы.
 
 https://en.wikipedia.org/wiki/Fibonacci_number
+https://en.wikipedia.org/wiki/Generalizations_of_Fibonacci_numbers
 """
+from itertools import islice
 
 
-def fibonacci(n: int) -> int:
-    if n < 0:
-        raise ValueError("Index must be >= 0")
+def fibonacci(num: int) -> int:
+    if int(num) != num:
+        raise ValueError('Index must be integer')
+    if num == 0:
+        """
+        Case was extracted in according
+        to generalizing generator for
+        negative numbers
+        """
+        return 0
+    mod_n = abs(num)
+    fib_gen = islice(fibonacci_gen(), mod_n - 1, mod_n)
+    sign_mul = 1
+    if num < 0:
+        sign_mul = (-1) ** ((mod_n - 1) % 2)
+    return sign_mul * next(fib_gen)
 
-    pass
+
+def fibonacci_gen():
+    prev, curr = 1, 1
+    yield from (prev, curr)
+    while True:  # noqa: WPS457
+        prev, curr = curr, prev + curr
+        yield curr
--- fibonacci/test_fibonacci.py
+++ fibonacci/test_fibonacci.py
@@ -1,17 +1,23 @@
 import pytest
 from fibonacci import fibonacci
+from math import log10
 
 
 def test_first_3():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
     assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
 
 
 def test_neganacci():
     assert fibonacci(-1) == 1
     assert fibonacci(-2) == -1
     assert fibonacci(-3) == 2
+
+
+def test_huge_fib():
+    assert sum(map(int, str(fibonacci(150_000)))) == 139_626
 
 
 def test_not_integer_error():
