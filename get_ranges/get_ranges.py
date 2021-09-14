774826989ba7fb57a6e71618b998e610e202c0d5
Al Rempel <alex@rmpl.of.by>
2021-09-14 17:15:14 +0300

    :heavy_check_mark: `get_ranges`

--- get_ranges/get_ranges.py
+++ get_ranges/get_ranges.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 
 """
-Реализовать функцию `get_ranges` которая “сворачивает” список неповторяющихся целых чисел, отсортированных по возрастанию:
+Реализовать функцию `get_ranges` которая “сворачивает” список неповторяющихся
+целых чисел, отсортированных по возрастанию:
 
 - [0, 1, 2, 3, 4, 7, 8, 10] -> "0-4, 7-8, 10"
 - [4, 7, 10] -> "4, 7, 10"
 - [2, 3, 8, 9]) -> "2-3, 8-9"
 """
+from itertools import islice
+from typing import List
 
 
-def get_ranges(l: list) -> str:
-    pass
+def get_ranges(seq: List[int]) -> str:
+    if not seq:
+        return ''
+    ranges = []
+    f_iter, c_iter = iter(seq), iter(seq)
+    first = next(c_iter)
+    for prev, curr in zip(f_iter, c_iter):
+        if curr - prev == 1:
+            continue
+        range_ = format_range(first, prev)
+        ranges.append(range_)
+        first = curr
+    range_ = format_range(first, curr)  # noqa: WPS441
+    ranges.append(range_)
+    return str.join(', ', ranges)
+
+
+def format_range(first, last) -> str:
+    if first == last:
+        return f'{first}'  # noqa: WPS305
+    return f'{first}-{last}'  # noqa: WPS305
--- get_ranges/test_get_ranges.py
+++ get_ranges/test_get_ranges.py
@@ -1,5 +1,5 @@
+from itertools import chain
 from get_ranges import get_ranges
-
 
 def test_empty():
     assert get_ranges([]) == ""
@@ -15,3 +15,9 @@ def test_no_ranges():
 
 def test_mixed():
     assert get_ranges([2, 3, 5, 8, 9, 11, 12, 13, 20]) == "2-3, 5, 8-9, 11-13, 20"
+
+
+def test_huge_range():
+    SCALE = 10_000_000
+    range_ = chain(range(1, SCALE + 1), range(SCALE + 2, 2 * SCALE + 1))
+    assert get_ranges(range_) == f'1-{SCALE}, {SCALE + 2}-{2 * SCALE}'
