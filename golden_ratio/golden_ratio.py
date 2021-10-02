36cfe69ea2d27506fbb39e1d21832969512f9afe
Al Rempel <alex@rmpl.of.by>
2021-10-02 11:53:47 +0300

    :heavy_check_mark: `golden_ratio` theoretically more precise way

--- golden_ratio/golden_ratio.py
+++ golden_ratio/golden_ratio.py
@@ -16,36 +16,59 @@ https://en.wikipedia.org/wiki/Golden_ratio#Relationship_to_Fibonacci_sequence
 
 # Golden Ratio
 from collections import deque
+from decimal import Decimal
+from fractions import Fraction
+from itertools import dropwhile
 from typing import Generator, Tuple
 
-GOLDEN = (1 + 5 ** 0.5) / 2
+
+def get_sqrt_5(precision: Decimal) -> Fraction:
+    """
+    Returns rational approximation of √5 constant
+    with minimal denominator according to Hurwitz's
+    theorem in field of number theory.
+    """
+    mantiss = Fraction(1, 4)
+    curr_prescision = dec_one = Decimal(1)  # noqa: WPS429
+    while curr_prescision > precision:
+        mantiss = 1 / (4 + mantiss)
+        curr_prescision = dec_one / Decimal(2 * mantiss.denominator ** 2)
+    return 2 + mantiss
 
 
 def fib_gen() -> Generator[int, None, None]:
     """
     Generator to produce Fibonacci numbers indefinitely.
     """
-    yield from ((prev := 1), (curr := 1))
+    yield from ((prev := 1), (curr := 1))  # noqa: WPS332
     while True:  # noqa: WPS457
         prev, curr = curr, prev + curr
         yield curr
 
 
-def fib_pair_gen() -> Generator[Tuple[int, int], None, None]:
-    fib = fib_gen()
+def fib_pair_gen(unary_fib_iter) -> Generator[Tuple[int, int], None, None]:
+    """
+    Produce fibonacci numbers pairs
+
+    It possible to get pairs direct from
+    fibonacci one-by-one generator but i
+    wanted trying to build more sophisticated
+    «architecture» to be able parametrize fibonacci source
+    """
+    fib = unary_fib_iter
     tail = deque((next(fib),), maxlen=2)
-    while True:
+    while True:  # noqa: WPS457
         tail.append(next(fib))
         yield (tail[0], tail[1])
 
 
-def fib_phi_calc(precision: float) -> Tuple[int, int]:
+def fib_phi_calc(precision: Decimal) -> Tuple[int, int]:
     """
-    Returns 2 consecutive Fibonacci numbers to calculate Golden Ratio with a
-    given precision.
+    Returns 2 consecutive Fibonacci numbers to calculate
+    Golden Ratio rational approximation with a given precision.
     """
-    fpg = fib_pair_gen()
-    while True:
-        fib_pair = next(fpg)
-        if abs(GOLDEN - fib_pair[1] / fib_pair[0]) <= precision:
-            return fib_pair
+    sqrt_5 = get_sqrt_5(precision)  # noqa: WPS114
+    precision_cond = 1 / (sqrt_5 * Fraction(*precision.as_integer_ratio()))
+    fpg = fib_pair_gen(fib_gen())
+    phi_rat_iter = dropwhile(lambda pair: pair[0] ** 2 < precision_cond, fpg)
+    return next(phi_rat_iter)
--- golden_ratio/test_golden_ratio.py
+++ golden_ratio/test_golden_ratio.py
@@ -1,17 +1,19 @@
+from decimal import Decimal
+
 from golden_ratio import fib_phi_calc
 
 
 def test_micro():
-    assert fib_phi_calc(0.000_001) == (987, 1597)
+    assert fib_phi_calc(Decimal('0.000_001')) == (987, 1597)
 
 
 def test_nano():
-    assert fib_phi_calc(0.000_000_001) == (28657, 46368)
+    assert fib_phi_calc(Decimal('0.000_000_001')) == (28657, 46368)
 
 
 def test_pico():
-    assert fib_phi_calc(10e-12) == (317811, 514229)
+    assert fib_phi_calc(Decimal('10e-12')) == (317811, 514229)
 
 
 def test_femto():
-    assert fib_phi_calc(10e-15) == (9227465, 14930352)
+    assert fib_phi_calc(Decimal('10e-15')) == (9227465, 14930352)
