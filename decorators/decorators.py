f76062e7497f29c6b041d67dce47f73936fa95e7
Al Rempel <alex@rmpl.of.by>
2021-10-07 18:48:37 +0300

    :heavy_check_mark: `decorators`

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -13,11 +13,47 @@ continue_on_errors со значением по умолчанию False.
 
 В случае любого несоответствия необходимо сгенерировать исключение TypeError.
 """
+from functools import wraps
 
 
-def type_check():
-    pass
+def type_check(*types):
+    def checker_wrapper(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            nonlocal types  # noqa: WPS420
+            if len(args) != len(types):
+                raise TypeError(
+                    f'Wrong arguments count: need {len(types)} but get {len(args)}',
+                    )
+            err_msgs = []
+            for idx, (arg, checked_type) in enumerate(zip(args, types)):
+                if not isinstance(arg, checked_type):
+                    err_msgs.append(
+                        f'arg {idx} need {checked_type} but get {type(arg)}',
+                        )
+            if err_msgs:
+                raise TypeError(
+                    f'Wrong arguments types: {";".join(err_msgs)}'
+                    )
+            return func(*args, **kwargs)
+        return wrapped
+    return checker_wrapper
 
 
-def repeat():
-    pass
+def repeat(repeats=2, continue_on_errors=False):
+    def repeats_wrapper(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            nonlocal repeats
+            nonlocal continue_on_errors
+            rep_count = repeats
+            while rep_count > 0:
+                rep_count -= 1
+                try:
+                    _ = func(*args, **kwargs)
+                except Exception as e:
+                    if not continue_on_errors:
+                        break
+                    continue
+        return wrapped
+    return repeats_wrapper
